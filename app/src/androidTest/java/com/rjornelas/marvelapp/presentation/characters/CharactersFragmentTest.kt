54ea4ae9c88ca3ab1c207ac15acf146cee33268b
rjornelas <rafael.j.ornelas@gmail.com>
2024-04-15 20:40:17 -0300

    Removed generic imports

--- app/src/androidTest/java/com/rjornelas/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/rjornelas/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -3,7 +3,9 @@ package com.rjornelas.marvelapp.presentation.characters
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.rjornelas.marvelapp.R
 import com.rjornelas.marvelapp.extension.asJsonString
